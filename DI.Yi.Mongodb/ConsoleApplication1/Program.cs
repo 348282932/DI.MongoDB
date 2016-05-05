71b572456089cf696c110c250d95fc58f07526c4
HuJiaming <hujiaming@szpayu.com>
2016-05-05 20:39:12 +0800

    初始化提交

---
+++ DI.Yi.Mongodb/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using DI.Yi.Mongodb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var mongoDbServer = new MongoDBServer();
+
+			mongoDbServer.Init();
+		}
+	}
+}
